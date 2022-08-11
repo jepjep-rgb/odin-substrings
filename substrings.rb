0d95620dafeb44c5eefbe75a51a0ce0e95f1a102
jepjep-rgb <jefroyalde@gmail.com>
2022-08-11 12:58:55 +0800

    Implement method to record data on matched words
    The substring array takes an array containing the key-value pair.
    The function returns the hash of the substring array.

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,11 @@
 def substrings(string, dictionary)
+  substring = []
   dictionary.each do |word|
     regexp = Regexp.new(word)
     match_count = string.scan(regexp).length
+    if match_count > 0
+      substring.push([word, match_count])
+    end
   end
+  substring.to_h
 end
